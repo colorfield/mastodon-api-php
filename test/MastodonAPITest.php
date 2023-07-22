23d991279d2c20844e19f93ca88bdd4c1de2dd83
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-07-22 15:19:51 +0200

    test: verify credentials

--- test/MastodonAPITest.php
+++ test/MastodonAPITest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Assert;
+use Dotenv\Dotenv;
 use Colorfield\Mastodon\OAuthScope;
 use Colorfield\Mastodon\MastodonOAuth;
 use Colorfield\Mastodon\MastodonAPI;
@@ -15,14 +16,14 @@ final class MastodonAPITest extends TestCase
 {
 
     /**
-     * @var \Colorfield\Mastodon\MastodonAPI;
-     */
-    protected $api;
-
-    /**
-     * @var \Colorfield\Mastodon\MastodonOAuth
+     * @var MastodonOAuth
      */
     protected $oAuth;
+
+    /**
+     * @var MastodonAPI;
+     */
+    protected $api;
 
     /**
      * {@inheritDoc}
@@ -32,15 +33,29 @@ final class MastodonAPITest extends TestCase
         $name = 'MyMastodonApp';
         $instance = 'mastodon.social';
         $this->oAuth = new MastodonOAuth($name, $instance);
+
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+
+        $this->oAuth->config->setClientId($_ENV['CLIENT_ID']);
+        $this->oAuth->config->setClientSecret($_ENV['CLIENT_SECRET']);
+        $this->oAuth->config->setBearer($_ENV['BEARER']);
+        $this->api = new Colorfield\Mastodon\MastodonAPI($this->oAuth->config);
     }
 
-    // @todo test OAuth
-    // @todo test Mastodon API
-
-    public function testDefaultAuthorizationUrl()
+    public function testOAuthDefaultAuthorizationUrl()
     {
         $authorizationUrl = $this->oAuth->getAuthorizationUrl();
         Assert::assertTrue(str_starts_with($authorizationUrl, 'https://mastodon.social/oauth/authorize/?response_type=code&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=read+write&client_id='));
+    }
+
+    public function testApiVerifyCredentials()
+    {
+        $credentials = $this->api->get('/accounts/verify_credentials');
+        $user = new Colorfield\Mastodon\UserVO($credentials);
+        $hasUserName = !empty($user->username);
+        $hasUserId = !empty($user->id);
+        Assert::assertTrue($hasUserName && $hasUserId);
     }
 
 }
